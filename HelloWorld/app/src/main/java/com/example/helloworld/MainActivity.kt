fd3d2cb86b00e010c4f87867701ae19665cbea47
Zhijie Kuang <zhijiekuangpersonal@gmail.com>
2022-07-11 19:01:13 -0400

    Updated app with opt features and updated README

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.res.ColorStateList
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -18,7 +19,9 @@ class MainActivity : AppCompatActivity() {
 
         // Show this layout file: activity_main
         setContentView(R.layout.activity_main)
-
+        //Variables
+        val displayTxt = findViewById<TextView>(R.id.textView)
+        val accessBG = findViewById<ConstraintLayout>(R.id.backgroundView)
         // Button Interactions
         // 1. Get a reference to button (change text color)
         findViewById<Button>(R.id.button).setOnClickListener {
@@ -26,7 +29,7 @@ class MainActivity : AppCompatActivity() {
             Log.i("Zhijie", "Tapped on button")
             // Change the color of the text
             // Get a reference to the text view
-            findViewById<TextView>(R.id.textView).setTextColor(getResources().getColor(R.color.black))
+            displayTxt.setTextColor(getResources().getColor(R.color.black))
             // Changes the text color in edittext as well
             findViewById<EditText>(R.id.et_simple).setTextColor(getResources().getColor(R.color.black))
 
@@ -35,7 +38,7 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.button2).setOnClickListener {
             // Change the color of the background
             // Get a reference to the background view
-            findViewById<ConstraintLayout>(R.id.backgroundView).setBackgroundColor(
+            accessBG.setBackgroundColor(
                 getResources().getColor(
                     androidx.appcompat.R.color.material_grey_300
                 )
@@ -45,8 +48,24 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.button3).setOnClickListener {
             // Get a reference to the EditText
             val inputText:EditText=findViewById(R.id.et_simple)
-            val showTxt = inputText.text.toString()
-            findViewById<TextView>(R.id.textView).text = showTxt
+            var showTxt = inputText.text.toString()
+            if (showTxt == ""){
+                showTxt = "Hello from Zhijie"
+            }
+            displayTxt.text = showTxt
+            //Closes keyboard
+            inputText.onEditorAction(EditorInfo.IME_ACTION_DONE)
+        }
+        // 4. Get a reference to button (change text to goodbye)
+        findViewById<Button>(R.id.button4).setOnClickListener {
+            displayTxt.text = "Goodbye \uD83D\uDC4B"
+        }
+        // 5. Get a reference to button (reset all to default)
+        findViewById<Button>(R.id.button5).setOnClickListener {
+            displayTxt.text = "Hello from Zhijie"
+            displayTxt.setTextColor(getResources().getColor(R.color.white))
+            accessBG.setBackgroundColor(getResources().getColor(androidx.cardview.R.color.cardview_dark_background))
+            findViewById<EditText>(R.id.et_simple).setTextColor(getResources().getColor(R.color.white))
         }
     }
 }
